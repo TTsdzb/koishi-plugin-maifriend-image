8485048208b6eb641885ef370dff93ecdfe15421
TTsdzb <ttsdzb@outlook.com>
2023-05-28 11:48:57 +0800

    fix: cannot load frame image
    Use local image instead of web ones to prevent network problems.
    
    fix #1

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { Context, Schema, h } from "koishi";
+import path from "path";
 
 export const name = "maifriend-image";
 
@@ -7,8 +8,42 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
+  const imagePath = "file://" + path.resolve(__dirname, "frame.png");
   const sendImage = "请发送图片。图片宽高比最好为1:1，否则只保留中间部分。";
   const pendings = new Set<string>();
+
+  function generate(imageUrl: string) {
+    return (
+      <html>
+        <div
+          style={{
+            width: "1170px",
+            height: "1162px",
+          }}
+        >
+          <img
+            style={{
+              position: "absolute",
+              left: "158px",
+              top: "190px",
+              width: "854px",
+              height: "854px",
+              "object-fit": "cover",
+            }}
+            src={imageUrl}
+          />
+          <img
+            style={{
+              position: "absolute",
+              left: 0,
+              top: 0,
+            }}
+            src={imagePath}
+          />
+        </div>
+      </html>
+    );
+  }
 
   ctx
     .command("maifriend [image:text]", "生成maimai旅行伙伴图片")
@@ -35,37 +70,4 @@ export function apply(ctx: Context) {
     pendings.delete(session.gid + session.uid);
     return generate(code.attrs.url);
   });
-}
-
-function generate(imageUrl: string) {
-  return (
-    <html>
-      <div
-        style={{
-          width: "1170px",
-          height: "1162px",
-        }}
-      >
-        <img
-          style={{
-            position: "absolute",
-            left: "158px",
-            top: "190px",
-            width: "854px",
-            height: "854px",
-            "object-fit": "cover",
-          }}
-          src={imageUrl}
-        />
-        <img
-          style={{
-            position: "absolute",
-            left: 0,
-            top: 0,
-          }}
-          src={"https://img1.imgtp.com/2023/04/27/0415eqID.png"}
-        />
-      </div>
-    </html>
-  );
 }
